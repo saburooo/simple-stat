c7f3f9f00a3246af4474dac3a14a0a4b87acb1bb
harutaakihisa <65touhei@gmail.com>
2021-02-07 20:09:56 +0900

    グラフの作成に着手する。

--- src/Chart.elm
+++ src/Chart.elm
@@ -15,11 +15,24 @@ import TypedSvg.Attributes exposing (strokeWidth)
 import TypedSvg.Attributes exposing (transform)
 
 
+-- グラフを作成するためにタイプを生成
+type Graph
+        = Xscale
+        | Yscale
+
+
+type GraphType
+        = Line
+        | Histogram
+
+
 -- SVG
+
+
 listVisualizeArgOne: List Float -> Svg.Svg msg
 listVisualizeArgOne floatList =
     let
-        floatListMap = List.map (\x -> x ^ 2 * 100) <| floatList
+        floatListMap = List.map (\x -> x * 100) <| floatList
         floatRange = List.map (\x -> toFloat x * 100) (List.range 0 (List.length floatList))
         tupleFloatList = List.map2 Tuple.pair floatRange floatListMap
     in
@@ -27,9 +40,13 @@ listVisualizeArgOne floatList =
             [ Svg.rect [ width (Types.percent 100), height (Types.percent 100), fill (Types.Paint Color.lightBlue) ] []
             , TypedSvg.g [ transform [ Types.Translate 0 100 ] ]
                 [ Svg.polyline [ points tupleFloatList
-                            , fill ( Types.Paint Color.blue )
+                            , fill ( Types.PaintNone )
                             , stroke (Types.Paint Color.blue)
                             , TypedSvg.Attributes.style "transform: scale(1, -1)"
                             , strokeWidth (Types.px 6) ] [] 
                 ]
             ]
+
+
+-- TODO 上記のタイプを使ったグラフのView関数を制作する。
+
